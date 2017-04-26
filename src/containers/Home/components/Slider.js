ce12c230e0bd417470b08f99859965f3da2c7ad6
John Son <jsson77@gmail.com>
2017-04-26 16:57:26 -0700

    adding assets to carousel

--- src/containers/Home/components/Slider.js
+++ src/containers/Home/components/Slider.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Slider from 'react-slick';
 
+import "../../../../dist/assets/camden/camden_interior1";
+import camdenMasterBath from './camden_masterBath.jpg';
+
 export default class SimpleSlider extends React.Component {
   render() {
     let settings = {
@@ -13,10 +16,14 @@ export default class SimpleSlider extends React.Component {
 
     return (
       <Slider {...settings}>
-        <div><h3>1</h3></div>
-        <div><h3>2</h3></div>
-        <div><h3>3</h3></div>
-        <div><h3>4</h3></div>
+        <div><h3>[OUTSIDE] Chambers, Oakland CA</h3></div>
+        <div><h3>[INSIDE] Chambers, Oakland CA</h3></div>
+        <div><h3>[OUTSIDE] Camden, Oakland CA</h3></div>
+        {/* <div><h3>[INSIDE] Camden, Oakland CA</h3></div> */}
+        <div><img src={camdenMasterBath} alt="Camden Master Bath0000"/></div>
+        <div><img src="./camden_masterBath.jpg" alt="Camden Master Bath"/></div>
+        <div><img src="../../../../dist/assets/camden_interior1" alt="Camden Living Room"/></div>
+        <div><img src="http://i.imgur.com/TOCfkRg.jpg" alt="Camden Living Room"/></div>
         <div><h3>5</h3></div>
         <div><h3>6</h3></div>
       </Slider>
